4e769ff04484e3f3562347b156160e21014e9e67
YuxiangWang_0525(John Frederick Wang) <66881174+YuxiangWang0525@users.noreply.github.com>
2024-08-18 16:31:44 +0800

    add options api

--- src/renderer/src/user_runtime/main.vue
+++ src/renderer/src/user_runtime/main.vue
@@ -2,11 +2,25 @@
 
 </template>
 <script setup lang="ts">
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 // Write your user component runtime here
 // You can directly import VueUse and Vue-Router to use, or you can introduce other third-party libraries.
 // Please note: If you want to use a state management library, you should use Pinia instead of Vuex because it is incompatible with DoConnect.
 import { onMounted } from 'vue'
 onMounted(() => {
 
+})
+</script>
+<script lang="ts">
+// if you want to use the options api, you can use write it here
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup(){
+    return {
+
+    }
+  }
 })
 </script>
